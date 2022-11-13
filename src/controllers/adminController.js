2cf246c0c3859831f68e99d6b9daca4e1553d39d
JunarisAlf <junaris0715@gmail.com>
2022-11-13 21:14:24 +0700

    Add getAdmin and adminUpdate function

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,28 +1,70 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const {Admin} = require('../db/models')
+const {Admin} = require('../db/models');
 const response = require('../utils/response');
 
-module.exports = class adminController{
-    static async login(req, res, next){
+module.exports = class adminController {
+    static async login(req, res, next) {
         const {username, password} = req.body;
-        try{
-            const adminRes = await Admin.findOne({where: { username}})
-            if(adminRes == null) return next({name: 'UserNotFound'})
+        try {
+            const adminRes = await Admin.findOne({where: {username}});
+            if (adminRes == null) return next({name: 'UserNotFound'});
             const validUser = await bcrypt.compare(password, adminRes.password);
-            if(validUser){
+            if (validUser) {
                 const jwtToken = jwt.sign(
                     {role: 'admin', id: adminRes.id, username},
                     process.env.JWT_SECRET
                 );
-                res.status(200).json(response(200, true, "Login Successfull", {token: jwtToken}));
+                return res.status(200).json(
+                    response(200, true, 'Login Successfull', {token: jwtToken})
+                );
             }
-            next({name: 'WrongPassword'})
-
-        }catch(err){
-            next(err)
+            next({name: 'WrongPassword'});
+        } catch (err) {
+            next(err);
         }
     }
-
-    
-}
+    static async getAdmin(req, res, next) {
+        const adminID = req.user.id;
+        try {
+            const {id, name, username} = await Admin.findOne({
+                where: {id: adminID},
+            });
+            res.status(200).json(
+                response(200, true, 'Data admin berhasil didapat', {
+                    id,
+                    name,
+                    username,
+                })
+            );
+        } catch (err) {
+            next(err);
+        }
+    }
+    static async updateAdmin(req, res, next) {
+        const {username, name} = req.body;
+        const jwtData = req.user; // (role, id, username)
+        try {
+            const adminRes = await Admin.update(
+                {
+                    username,
+                    name,
+                },
+                {where: {id: jwtData.id}, returning: true, plain: true}
+            );
+            const jwtToken = jwt.sign(
+                {role: 'admin', id: adminRes.id, username: adminRes.username},
+                process.env.JWT_SECRET
+            );
+            res.status(200).json(
+                response(200, true, 'Data admin berhasil di update', {
+                    token: jwtToken,
+                    id: adminRes[1].id,
+                    username: adminRes[1].username,
+                })
+            );
+        } catch (err) {
+            next(err);
+        }
+    }
+};
--- src/routes/admin-routes.js
+++ src/routes/admin-routes.js
@@ -7,24 +7,22 @@ const adminMiddleware = require('../middleware/adminMiddleware');
 
 router.post('/login', adminController.login);
 router.use(adminMiddleware);
-router.get('/', (req, res) => res.send('Admin Home'));
+router.get('/', adminController.getAdmin); //get admin data
+router.put('/', adminController.updateAdmin); //update data admin
+
 // driver
-router.post('/driver', driverController.createDriver) //create driver
-router.put('/driver/password/:driverID', driverController.updatePassword) //update password driver by admin
-router.put('/driver/:driverID', driverController.updateDriver) //update driver
-router.delete('/driver/:driverID', driverController.deleteDriver) //delete driver
-router.get('/driver', driverController.getAllDriver) //get all driver
-router.get('/driver/:driverID', driverController.getOneDriver) //get one driver
-
-
+router.post('/driver', driverController.createDriver); //create driver
+router.put('/driver/password/:driverID', driverController.updatePassword); //update password driver by admin
+router.put('/driver/:driverID', driverController.updateDriver); //update driver
+router.delete('/driver/:driverID', driverController.deleteDriver); //delete driver
+router.get('/driver', driverController.getAllDriver); //get all driver
+router.get('/driver/:driverID', driverController.getOneDriver); //get one driver
 
 // location
-router.post('/location', locationController.createLocation) //create location
-router.put('/location/:locID', locationController.updateLocation) //Update location
+router.post('/location', locationController.createLocation); //create location
+router.put('/location/:locID', locationController.updateLocation); //Update location
 
-router.get('/location', locationController.getAllLocation) //getAll location
-router.delete('/location/:locID', locationController.deleteLocation) //delete location
-
-
+router.get('/location', locationController.getAllLocation); //getAll location
+router.delete('/location/:locID', locationController.deleteLocation); //delete location
 
 module.exports = router;
