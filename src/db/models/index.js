b97bb65b786ded7e327a8b3922bc3dd5a936e9c6
JunarisAlf <junaris0715@gmail.com>
2022-11-30 16:14:37 +0700

    fix

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -10,7 +10,7 @@ const config = require('../../config/database')[env];
 const db = {};
 
 
-sequelize = new Sequelize(config.database, config.username, config.password, config);
+let  = new Sequelize(config.database, config.username, config.password, config);
 
 fs
   .readdirSync(__dirname)
